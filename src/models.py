768d8b0e78e467a338161a30ad17477b0173c9b0
Leonid ocampo <115747563+leonidocampo-co@users.noreply.github.com>
2023-05-17 22:39:41 +0000

    cabmios

--- src/models.py
+++ src/models.py
@@ -13,6 +13,35 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    dob = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    #film_id = Column(String(250), ForeignKey("films.id"))
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    height = Column(float(250), nullable=False)
+    homeworld_id = Column(Integer, ForeignKey("planets.id"))
+    planet = relationship("Planets")
+
+class Cast(Base):
+    __tablename__ = 'cast'
+    id = Column(Integer, primary_key=True)
+    film_id = Column(Integer, foreignKey("films.id"))
+    film = relationship("Films", backref="cast")
+    people_id = Column(Integer, ForeignKey("people.id"))
+    people = relationship("People")
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable=False)
+    weather = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    population = Column(float(250), nullable=False)
+    orbital_period = Column(float(250), nullable=False)
+
+
+
 
 class Address(Base):
     __tablename__ = 'address'
